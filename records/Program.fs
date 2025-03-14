f056f46b1152eab78be141bad226109fd3f85e6b
Leonardo Valencia <leov@gammawizard.com>
2025-03-13 20:16:06 -0400

    Using records

---
+++ records/Program.fs
@@ -0,0 +1,46 @@
+﻿//
+// Ejemplo de records
+//
+
+type Atlas = {
+    Pais: string
+    Capital: string
+    Habitantes: int
+}
+
+//
+// Base de datos actualizada usando records
+//
+let atlas =
+    [ 
+        {
+            Pais = "Colombia"
+            Capital = "Bogota"
+            Habitantes = 52320000
+        }
+        {
+            Pais = "Francia" 
+            Capital = "Paris"
+            Habitantes = 68290000
+        }
+        {
+            Pais = "España" 
+            Capital = "Madrid" 
+            Habitantes = 48350000
+        }
+        {
+            Pais = "Azerbaiyán" 
+            Capital = "Baku"
+            Habitantes = 10150000
+        }
+        {
+            Pais = "Alemania" 
+            Capital = "Berlin"; 
+            Habitantes = 83280000
+        }
+        {
+            Pais = "Japon"; 
+            Capital = "Tokyo"; 
+            Habitantes = 124500000
+        }
+    ]
