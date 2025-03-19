b710fdae542a890950b8a9a6cb1eb77abe066814
Leonardo Valencia <leov@gammawizard.com>
2025-03-18 20:36:38 -0400

    Ejemplo Basico

---
+++ unions/Program.fs
@@ -0,0 +1,36 @@
+﻿//
+// A game of cards
+//
+
+//
+// Este es un discriminated union
+//
+type Pinta =
+    | Corazones
+    | Diamantes
+    | Picas
+    | Treboles
+
+
+let suitUno = Corazones
+let suitDos = Diamantes
+
+let imprimirPinta carta  =
+    match carta with
+    | Corazones -> printfn "La Pinta es Corazones"
+    | Diamantes -> printfn "La Pinta es Diamantes"
+    | _ -> printfn "Aun no se que pinta es"
+
+//
+// Pasando los valores asignados
+//
+imprimirPinta suitUno
+imprimirPinta suitDos
+
+//
+// Pasando el valor directo
+//
+imprimirPinta Picas 
+imprimirPinta Treboles
+
+
