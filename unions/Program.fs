814e85179e867c3d412e30bbc0d97f126009a1e9
Leonardo Valencia <leov@gammawizard.com>
2025-03-19 21:29:09 -0400

    Completando el juego de cartas

--- unions/Program.fs
+++ unions/Program.fs
@@ -15,34 +15,80 @@ type CartaDeJuego =
     | As of Pinta
     | Rey of Pinta
     | Reina of Pinta
-    | Sota of Pinta
+    | Jack of Pinta
     | CartaNumero of int * Pinta // Quizz, que tyipo es este?
 
+//
+// Helper functions for the big homework
+//
 
-let suitUno = Corazones
-let suitDos = Diamantes
+//
+// Esta funcion nos retorna la pinta de cuaquier carta
+//
 
-let cartaUno = Rey Diamantes
-let cartaDos = As Corazones
+let obtenerPintaDeCarta carta =
+    match carta with 
+    | As pinta -> pinta
+    | Rey pinta -> pinta
+    | Reina pinta -> pinta
+    | Jack pinta -> pinta
+    | CartaNumero(_,pinta) -> pinta
 
-let cartaTres = CartaNumero(8,Treboles)
+let result = obtenerPintaDeCarta (CartaNumero(10,Corazones))
+printfn $"La pinta de la carta es: {result}"
 
-let imprimirPinta carta  =
+//
+// Funcion que chequea si una lista de cartas es de la misma
+// pinta
+//
+let mismaPintaEnLista listaCartas =
+    //
+    // La pinta de muestra la sacamos del primer elemento de la lista
+    //
+    let pintaMuestra = listaCartas|> List.head |> obtenerPintaDeCarta
+
+    //
+    // Usamos forall para comprobar 
+    listaCartas
+    |> List.forall(fun e -> obtenerPintaDeCarta(e) = pintaMuestra)
+
+let test = mismaPintaEnLista [As(Treboles);CartaNumero(10,Corazones);Rey(Treboles);CartaNumero(3,Corazones)]
+printfn $"Misma pinta es {test}"
+
+//
+// Obtener valor de la carta, esto es util para
+// ordernar la mano
+//
+let obtenerValorDeCarta carta =
     match carta with
-    | Corazones -> printfn "La Pinta es Corazones"
-    | Diamantes -> printfn "La Pinta es Diamantes"
-    | _ -> printfn "Aun no se que pinta es"
+    | CartaNumero(x,_) -> x
+    | Jack(_) -> 11
+    | Reina(_) -> 12
+    | Rey(_) -> 13
+    | As(_)-> 14
+
 
 //
-// Pasando los valores asignados
+// Esta funcion da un valor arbitrario a cada suit
 //
-imprimirPinta suitUno
-imprimirPinta suitDos
 
-//
-// Pasando el valor directo
-//
-imprimirPinta Picas 
-imprimirPinta Treboles
+let obtenerValorDePinta carta =
+    match carta |> obtenerPintaDeCarta with
+    | Treboles -> 1
+    | Picas -> 2
+    | Corazones -> 3
+    | Diamantes -> 4
 
+let compararCartas carta1 carta2 =
+    let pinta1 = carta1 |> obtenerValorDePinta
+    let pinta2 = carta2 |> obtenerValorDePinta
+  
+    let c1 = compare pinta1 pinta2
+    if c1 <> 0 
+        then
+            c1
+        else
+            let valor1 = carta1 |> obtenerValorDeCarta
+            let valor2 = carta2 |> obtenerValorDeCarta
+            compare valor1 valor2
 
