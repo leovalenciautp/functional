a85218205561cba4f2e667dcb4b9970945e47365
Leonardo Valencia <leov@gammawizard.com>
2025-03-27 19:51:38 -0400

    Primera version de aplicacion de mapas

---
+++ map/Program.fs
@@ -0,0 +1,8 @@
+﻿let miMapa = [(1,"Cali");(2,"Bogota");(3,"Medellin")] |> Map.ofList
+
+//
+// Vamos a adicionar una ciudad mas
+// Recuerden que en F# todo es inmutable
+// asi que un nuevo mapa es retornado
+//
+let nuevoMapa = miMapa |> Map.add 3 "Pereira"
