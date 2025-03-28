cd0426a042e998dc69190b534a4a2b552c237ebd
Leonardo Valencia <leov@gammawizard.com>
2025-03-27 20:44:39 -0400

    Implementando un ReadLine para usar en Fable

---
+++ input/Program.fs
@@ -0,0 +1,8 @@
+﻿//
+// Este codigo es un hack para poder leer informacion
+// del teclado desde Fable
+//
+open Fable.Core
+
+[<Emit("prompt($0)")>]
+let ReadLine (mensaje:string) : string = jsNative
