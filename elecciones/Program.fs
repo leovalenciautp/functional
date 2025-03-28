0c8fe98f61ee94656e0a24b6a0589f59722c8cf6
Leonardo Valencia <leov@gammawizard.com>
2025-03-27 22:30:52 -0400

    Estad inicial de elecciones

---
+++ elecciones/Program.fs
@@ -0,0 +1,46 @@
+﻿type Candidato =
+    {
+        Cedula: int
+        Nombre: string
+        Partido: string
+    }
+
+type EstadoVoto =
+    | Marcado
+    | EnBlanco
+
+type Tarjeton =
+    {
+        EstadoUno: EstadoVoto
+        EstadoDos: EstadoVoto
+        EstadoTres: EstadoVoto
+    }
+
+let candidatos =
+    [
+        {Cedula = 101; Nombre = "JFK"; Partido ="Democrata"}
+        {Cedula = 273; Nombre = "Guillermo Leon Valencia"; Partido="Liberal"}
+        {Cedula = 564; Nombre = "Ronald Reagan";Partido ="Republicano"}
+    ]
+
+let mapaCandidato = [(1,273);(2,564);(3,101)] |> Map.ofList
+
+let resultados =
+    [
+        {EstadoUno = Marcado;EstadoDos=EnBlanco;EstadoTres=EnBlanco}
+        {EstadoUno = Marcado;EstadoDos=Marcado;EstadoTres=EnBlanco}
+        {EstadoUno = EnBlanco;EstadoDos=EnBlanco;EstadoTres=EnBlanco}
+        {EstadoUno = EnBlanco;EstadoDos=EnBlanco;EstadoTres=EnBlanco}
+        {EstadoUno = EnBlanco;EstadoDos=Marcado;EstadoTres=EnBlanco}
+        {EstadoUno = Marcado;EstadoDos=EnBlanco;EstadoTres=EnBlanco}
+        {EstadoUno = Marcado;EstadoDos=EnBlanco;EstadoTres=EnBlanco}
+        {EstadoUno = Marcado;EstadoDos=EnBlanco;EstadoTres=EnBlanco}
+        {EstadoUno = EnBlanco;EstadoDos=Marcado;EstadoTres=EnBlanco}
+        {EstadoUno = EnBlanco;EstadoDos=EnBlanco;EstadoTres=Marcado}
+        {EstadoUno = EnBlanco;EstadoDos=EnBlanco;EstadoTres=Marcado}
+        {EstadoUno = EnBlanco;EstadoDos=EnBlanco;EstadoTres=Marcado}
+        {EstadoUno = EnBlanco;EstadoDos=EnBlanco;EstadoTres=Marcado}
+        {EstadoUno = EnBlanco;EstadoDos=Marcado;EstadoTres=EnBlanco}
+        {EstadoUno = EnBlanco;EstadoDos=Marcado;EstadoTres=EnBlanco}
+        {EstadoUno = EnBlanco;EstadoDos=EnBlanco;EstadoTres=Marcado}
+    ]
