b68cdfe03af55330346c19d07b321471ceca568a
Leonardo Valencia <leov@gammawizard.com>
2025-03-27 20:00:49 -0400

    Construyendo un map con una base de datos

--- map/Program.fs
+++ map/Program.fs
@@ -6,3 +6,28 @@
 // asi que un nuevo mapa es retornado
 //
 let nuevoMapa = miMapa |> Map.add 3 "Pereira"
+
+type Ciudadano =
+    {
+        Cedula: int
+        Nombre: string
+    }
+
+let miPais = 
+    [
+        {Cedula = 1; Nombre = "Leonardo Valencia"}
+        {Cedula = 2; Nombre = "Carlos Meneses"}
+        {Cedula = 3; Nombre = "Luis Carlos Gonzales"}
+    ]
+
+//
+// Vamos a construir un mapa para buscar rapido por
+// cedula
+//
+
+let fastAccess =
+    miPais
+    |> List.fold (fun acc e -> acc |> Map.add e.Cedula e) Map.empty
+
+fastAccess
+|> Map.iter (fun key value -> printfn $"Key: {key} Value: {value}")
