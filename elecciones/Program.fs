497f75af46318cb15b5f8a8f73126008eecbd282
Leonardo Valencia <leov@gammawizard.com>
2025-04-01 22:04:56 -0400

    Finalizando programa de elecciones

--- elecciones/Program.fs
+++ elecciones/Program.fs
@@ -25,8 +25,6 @@ let candidatos =
         {Cedula = 564; Nombre = "Ronald Reagan";Partido ="Republicano"}
     ]
 
-let mapaCandidato = [(1,273);(2,564);(3,101)] |> Map.ofList
-
 let resultados =
     [
         {EstadoUno = Marcado;EstadoDos=EnBlanco;EstadoTres=EnBlanco}
@@ -54,33 +52,56 @@ type Resultado =
     | Invalido
     | Blanco
 
+let mapaCandidato = [(CandidatoUno,273);(CandidatoDos,564);(CandidatoTres,101)] |> Map.ofList
+
 let resultadosIniciales =
     [
-        (CandidatoUno,0)
-        (CandidatoDos,0)
-        (CandidatoTres,0)
-        (Invalido,0)
-        (Blanco,0)
+        CandidatoUno,0
+        CandidatoDos,0
+        CandidatoTres,0
+        Invalido,0
+        Blanco,0
     ]
     |> Map.ofList
 
-let incrementarVoto acumulador key =
+let incrementarVoto acumulador (key:Resultado) =
     let total = acumulador |> Map.find key
     acumulador |> Map.add key (total+1)
 
 let procesarVoto acumulador voto =
     let incremente = incrementarVoto acumulador
 
-    match (voto.EstadoUno,voto.EstadoDos,voto.EstadoTres) with
-    | (EnBlanco,EnBlanco,EnBlanco) -> incremente Blanco
-
-    | (Marcado, EnBlanco,EnBlanco) -> incremente CandidatoUno
-
-    | (EnBlanco,Marcado,EnBlanco) -> incremente CandidatoDos
-    | (EnBlanco,EnBlanco,Marcado) -> incremente CandidatoTres
+    match voto.EstadoUno,voto.EstadoDos,voto.EstadoTres with
+    | EnBlanco,EnBlanco,EnBlanco -> incremente Blanco
+    | Marcado, EnBlanco,EnBlanco -> incremente CandidatoUno
+    | EnBlanco,Marcado,EnBlanco -> incremente CandidatoDos
+    | EnBlanco,EnBlanco,Marcado -> incremente CandidatoTres
     | _ -> incremente Invalido
 
 let totalizarVotos votos =
     votos
     |> List.fold procesarVoto resultadosIniciales
+
+let encontrarInformacionCandidato key =
+    mapaCandidato 
+    |> Map.find key
+    |> fun cedula ->
+        candidatos |> List.find (fun r -> r.Cedula = cedula)
+    
+
+let convertirANombreYVotos (key,votos) =
+   match key with
+        | Blanco -> "En Blanco",votos
+        | Invalido -> "Voto Invalido",votos
+        | _ -> 
+                let info = encontrarInformacionCandidato key
+                info.Nombre+" ** "+info.Partido,votos 
+
+resultados
+|> totalizarVotos
+|> Map.toArray
+|> Array.sortByDescending (fun (_,votos) -> votos)
+|> Array.map convertirANombreYVotos
+|> Array.iter (fun (nombre,votos) -> printfn $"{nombre} -> {votos}" )
+
 
