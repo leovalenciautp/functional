ed5194300409978865041a2f52155c79fb912430
Leonardo Valencia <leov@gammawizard.com>
2025-04-01 22:50:58 -0400

    Agregando nested records

---
+++ recordavanzado/Program.fs
@@ -0,0 +1,23 @@
+﻿type NombreCompleto = {
+    PrimerNombre: string
+    SegundoNombre: string option
+    PrimerApellido: string
+    SegundoApellido: string option
+}
+
+type Directorio = {
+    Nombre: NombreCompleto
+    Telefono: int
+    Email: string
+}
+
+let directorio = [
+    {
+        Nombre = {PrimerNombre = "Leonardo";SegundoNombre=None;PrimerApellido="Valencia";SegundoApellido=Some "Olarte"}
+        Telefono = 2
+        Email = "leonardo.valencia@uto.edu.co"
+    }
+]
+
+let persona = directorio[0]
+printfn $"{persona.Nombre.PrimerNombre}"
