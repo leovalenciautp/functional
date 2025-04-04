39969a3b6d84202a94e5f26378aebf22eb9bbbc7
Leonardo Valencia <leov@gammawizard.com>
2025-04-03 22:28:04 -0400

    Ejemplo para el segundo parcial

---
+++ parcialdos/Program.fs
@@ -0,0 +1,46 @@
+﻿type Moneda =
+    | Dolar
+    | Pesos
+    | Euro
+    | Yuan
+
+type Balance =
+    Valor of Moneda * decimal
+
+type Cuenta = {
+    Banco: string
+    Saldo: Balance
+}
+
+let cuentas = [
+    {
+        Banco = "Bancolombia"
+        Saldo = Valor(Pesos,100000000m)
+    }
+    {
+        Banco = "HSBC"
+        Saldo = Valor(Yuan,500000m) 
+    }
+    {
+        Banco = "LA CAIXA"
+        Saldo = Valor(Euro,125000m)
+    }
+    {
+        Banco = "Citibank"
+        Saldo = Valor(Dolar,345000m)
+    }
+]
+
+let cambioMoneda (cantidad:Balance) =
+    match cantidad with 
+    | Valor(Dolar,x) -> x
+    | Valor(Pesos,x) -> x/4162m
+    | Valor(Euro,x) -> 1.1073m*x
+    | Valor(Yuan,x) -> x/7.2823m
+
+let totalizarCuentas cuentas =
+    cuentas
+    |> List.fold (fun total cuenta -> total + cambioMoneda cuenta.Saldo) 0m
+
+let portafolio = cuentas |> totalizarCuentas
+printfn $"{portafolio}"
--- recordavanzado/Program.fs
+++ recordavanzado/Program.fs
@@ -21,3 +21,25 @@ let directorio = [
 
 let persona = directorio[0]
 printfn $"{persona.Nombre.PrimerNombre}"
+
+type Colores =
+    | Blanco
+    | Rojo
+    | Negro
+    // En este ejemplo RGB espera una tupla de 3 valores enteros
+    | RGB of int*int*int 
+
+let colorUno = Colores.Blanco
+let colorDos = Rojo //No hay necesidad de usar Colores.Rojo
+
+//Aqui tenemos que darle valor a RGB
+let colorTres = RGB(255,255,0) 
+
+//
+// Usando match con el discriminated union
+//
+match colorTres with
+| Blanco -> printfn "El color es Blanco"
+| Rojo -> printfn "El color es Rojo"
+| Negro -> printfn "El color es Nego"
+| RGB(r,g,b) -> printfn $"Components {r} ** {g} ** {b}"
