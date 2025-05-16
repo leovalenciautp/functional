5645639d13e55acc873abafd867c9c63d0afbd8f
Leonardo Valencia <leov@gammawizard.com>
2025-05-16 11:03:30 -0400

    Adding the final example app

---
+++ files/Program.fs
@@ -0,0 +1,34 @@
+﻿open System.IO
+open System
+let mensaje = ["Esta es una demostracion";"de como grabar multiples lineas";"en un archivo"]
+
+//
+// Escribiendo las lineas en el archivo en una sola operacion
+//
+File.WriteAllLines("demo1.txt",mensaje)
+
+//
+// Aqui preguntamos linea por linea y luego la escribimo
+//
+let lineas =
+    Seq.initInfinite (
+        fun _ ->
+            printfn "Entre nombre, SALIR para salir"
+            Console.ReadLine()
+    )
+    |> Seq.takeWhile (fun e -> e <> "SALIR")
+    |> Seq.toList
+
+
+
+File.WriteAllLines("demo2.txt",lineas)
+
+
+//
+// Leyendo las lineas a ver si quedaron
+// bien.
+//
+let test = File.ReadAllLines("leo.txt")
+printfn $"{test.Length}"
+
+
---
+++ final/Program.fs
@@ -0,0 +1,68 @@
+﻿open System
+open System.IO
+let presentarMenu() =
+    Console.Clear()
+    Console.ForegroundColor <- ConsoleColor.Cyan
+    printfn "Bienvenido al proyecto final!!"
+    printfn "Elija una opcion:"
+    Console.ForegroundColor <- ConsoleColor.Yellow
+    printfn "********* 1 ***** Agregar Información"
+    printfn "********* 2 ***** Mostrar Información"
+    printfn "********* 3 ***** Salir"
+
+
+
+let agregarInformacion() =
+
+    Seq.initInfinite ( fun _ ->
+        printfn "Escribe SALIR para terminar"
+        printf "Entre un Nombre: "
+        Console.ReadLine()
+    )
+    |> Seq.takeWhile (fun linea ->
+        match linea with
+        | "SALIR" -> false
+        | _ -> true
+    )
+    |> Seq.toList
+    |> fun info -> File.AppendAllLines("datos.txt",info)
+
+
+let mostrarInfornacion() =
+    Console.ForegroundColor <- ConsoleColor.Green
+    File.ReadAllLines("datos.txt")
+    |> Seq.iter (fun linea ->
+        printfn $"{linea}"
+    )
+    Console.ForegroundColor <- ConsoleColor.Cyan
+    printfn "Presiona cualquier tecla para continuar"
+    Console.ReadKey() |> ignore
+    
+
+
+let mainLoop() =
+    Seq.initInfinite (fun _ ->
+        presentarMenu()
+        Console.ReadKey()
+    )
+    |> Seq.takeWhile ( fun o ->
+        match o.KeyChar with 
+        | '1' -> 
+            agregarInformacion()
+            true
+        | '2' -> 
+            mostrarInfornacion()
+            true
+        | '3' ->
+            printfn "Good bye!"
+            false
+        | _ ->
+            Console.ForegroundColor <- ConsoleColor.Red
+            printfn "Opcion erreda intente de nuevo"
+            printfn "Presiona cualquier teclar para continuar"
+            Console.ReadKey() |> ignore
+            true
+    )
+    |> Seq.toList
+
+mainLoop() |> ignore
